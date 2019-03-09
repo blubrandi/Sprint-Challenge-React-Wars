4365231ac9a3939c3ea38403369932dc09247864
blubrandi <blubrandi0@gmail.com>
2019-03-09 13:57:41 -0700

    Added created property as individual key for each character.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
     this.getCharacters('https://swapi.co/api/people/');
   }
 
-  getCharacters = URL => {
+   getCharacters = URL => {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
@@ -25,6 +25,7 @@ class App extends Component {
         return res.json();
       })
       .then(data => {
+        console.log(data)
         this.setState({ starwarsChars: data.results });
       })
       .catch(err => {
@@ -33,13 +34,17 @@ class App extends Component {
   };
 
   render() {
+    
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
         <div className="cardContainer">
-          {this.state.starwarsChars.map((character) => {
+          {this.state.starwarsChars.map((character, created) => {
               return (
-                <Character character={character} />
+                <Character 
+                character={character}
+                key={created}
+                />
               );
             })}
           </div>
